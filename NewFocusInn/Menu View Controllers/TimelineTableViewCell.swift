85369cab88d32aa2f0068dc1f9b0804e71b92d23
Dara Oseyemi <s012438@students.lmsd.org>
2019-02-22 12:31:19 -0500

    Continued working in timeline storyboard

---
+++ NewFocusInn/Menu View Controllers/TimelineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineTableViewCell.swift
+//  NewFocusInn
+//
+//  Created by Dara Oseyemi (student LM) on 2/21/19.
+//  Copyright © 2019 Dara Oseyemi (student LM). All rights reserved.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
