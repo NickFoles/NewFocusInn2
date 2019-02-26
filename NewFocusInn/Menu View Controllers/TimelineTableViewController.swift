c7e019380ca76b5b4a87fe94c99a8eb17bfc6743
Dara Oseyemi <s012438@students.lmsd.org>
2019-02-25 19:16:10 -0500

    GameScene 	- added Eiffel tower building to the city scene 	- created a new function to add the Eiffel tower Timeline 	- made some small changes to the timeline storyboard 	- added some code to create cells (work in progress) Images 	- got rid of some useless tiles and changes the size of some images

--- NewFocusInn/Menu View Controllers/TimelineTableViewController.swift
+++ NewFocusInn/Menu View Controllers/TimelineTableViewController.swift
@@ -8,40 +8,97 @@
 
 import UIKit
 
+extension Date {
+    var weekdayNameAndDate: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E/MMM d"
+        return formatter.string(from: self as Date)
+    }
+    
+    var time: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: self as Date)
+    }
+}
+
 class TimelineTableViewController: UITableViewController {
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet var timelineTableView: UITableView!
+    var totalTimelineItems = Int()
+    var dayTimelineItems = Int()
+    var rowNum = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        totalTimelineItems += dayTimelineItems
+        
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        dayTimelineItems = 1
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return totalTimelineItems
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        if dayTimelineItems == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Date Cell", for: indexPath) as! TimelineTableViewCell
+            
+            // Configure the cell...
+            cell.dateLabel.text = Date().weekdayNameAndDate
+            print("ran")
+            return cell
+        }
+        
+     /* else if newBuilding || destroyedBuilding {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Building Cell", for: indexPath) as! TimelineTableViewCell
+         
+            // Configure the cell...
+            cell.buildingTime.text = Date().time
+         
+            if newBuilding {
+                cell.buildingDescription = "Successfully constructed a \(timeConstructing)-minute \(buildingType)."
+            }
+            else {
+                cell.buildingDescription = "You've destroyed a \(buildingType)."
+            }
+         
+            cell.buildingImage.image = UIImage(named: "\(buildingType)")
+            return cell
+        }
+         
+         else if newAchievement {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Achievement Cell", for: indexPath) as! TimelineTableViewCell
+         
+            // Configure the cell...
+            cell.achievementTime.text = Date().time
+            cell.achievementDescription = "New achievement unlocked \(achievementType)."
+            cell.achievementImage.image = UIImage(named: "\(achievementType)")
+            return cell
+            }
+     */
+       return UITableViewCell()
     }
-    */
+    
+ 
 
     /*
     // Override to support conditional editing of the table view.
