0ad36d3d78b337d422b6d2996a9121de36372ee7
Dara Oseyemi <s012438@students.lmsd.org>
2019-04-10 13:12:34 -0400

    small stuff

--- NewFocusInn/AppDelegate.swift
+++ NewFocusInn/AppDelegate.swift
@@ -1,26 +1,14 @@
 //
-
 //  AppDelegate.swift
-
 //  NewFocusInn
-
 //
-
 //  Created by Dara Oseyemi (student LM) on 1/29/19.
-
 //  Copyright © 2019 Dara Oseyemi (student LM). All rights reserved.
-
 //
-
-
 
 import UIKit
-
 import Firebase
-
 import UserNotifications
-
-
 
 @UIApplicationMain
 
