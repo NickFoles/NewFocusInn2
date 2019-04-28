fa1c164fde9dc9cbf39d0a9f237d5c085ecf2dc9
Dara Oseyemi <s012438@students.lmsd.org>
2019-04-28 15:08:41 -0400

    The empire state building should now display in start focusing and focusing view controller.

--- NewFocusInn/Menu View Controllers/StartFocusingViewController.swift
+++ NewFocusInn/Menu View Controllers/StartFocusingViewController.swift
@@ -10,7 +10,7 @@ var globalTime : Double = 0
 
 var imageClicks = 0
 var firstRow = 0
-let buildingNames = [["house", "pink house", "news place"], ["ice cream store", "cinema", "petco"], ["tall building", "gray building", "wawa"], ["empire statee", "eiffel tower"]]
+let buildingNames = [["house", "pink house", "news place"], ["ice cream store", "cinema", "petco"], ["tall building", "gray building", "wawa"], ["empire state", "eiffel tower"]]
 
 import UIKit
 
